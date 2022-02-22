e53b14fcd24bafa71f2d73e25232aa98557c2912
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-02-22 07:21:06 +0300

    Update encoding test

--- src/test/java/EncodingTest.java
+++ src/test/java/EncodingTest.java
@@ -1,5 +1,13 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class EncodingTest {
+    Encoding testEncoding = new Encoding ("EDWIN MURIITHI", 2);
+    @Test
+    public void checkIfItCreatesInstance()
+    {
+        assertEquals(true, testEncoding instanceof Encoding);
+    }
 
 }
