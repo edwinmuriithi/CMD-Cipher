7d16de47fa9a8a363e24dfc69fcbfc62629a3993
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-02-22 07:14:11 +0300

    Edit Front End

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,6 +8,32 @@ public class App {
         int choice = scanner.nextInt();
         scanner.nextLine(); //throw away the \n not consumed by nextInt()
 
+        if(choice == 1)
+        {
+            System.out.println("Enter a text");
+            String inputText = scanner.nextLine().toUpperCase();
+            System.out.println("Enter a key ");
+            int key = scanner.nextInt();
+            Encoding encoding = new Encoding(inputText,key);
+            System.out.println(String.format("Your encrypted text is:  %s",encoding.encode()));
 
+        }
+        else if(choice == 2)
+        {
+            System.out.println("Enter an encrypted text to decrypt");
+            String inputText = scanner.nextLine().toUpperCase();
+            System.out.println("Enter a key ");
+            int key = scanner.nextInt();
+            Decoding decoding = new Decoding(key, inputText);
+            System.out.println(String.format("Your decrypted text is:  %s",decoding.decode()));
+        }
+        else if(choice == 3)
+        {
+            System.exit(0);
+        }
+        else
+        {
+            System.out.println("Input not known");
+        }
     }
 }
