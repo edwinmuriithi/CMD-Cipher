967f49d6679dbc5d721514e4b5ca5f0b041e5941
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-02-22 07:00:02 +0300

    Edit Decoding

--- src/main/java/Decoding.java
+++ src/main/java/Decoding.java
@@ -1,2 +1,34 @@
 public class Decoding {
+    private int mKey;
+    private String mInputText;
+
+    public Decoding(int key, String inputText) {
+        mKey = key;
+        mInputText = inputText;
+    }
+    public String getInputText() {
+        return mInputText;
+    }
+    public int getKey() {
+        return mKey;
+    }
+    public boolean isValidInputText()
+    {
+        char[] chars = mInputText.toCharArray();
+
+        for (char c : chars) {
+            if(!Character.isLetter(c) && c != ' ') {
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean isValidKey()
+    {
+        return mKey >1 && mKey < 26;
+    }
+
+    public String decode()
+    {
+
 }
