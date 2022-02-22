cf1996929a0f7375d7aa67cb4206e3a8c8102900
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-02-22 07:29:39 +0300

    edit decoding test

--- src/test/java/DecodingTest.java
+++ src/test/java/DecodingTest.java
@@ -1,5 +1,12 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DecodingTest {
-
+    Decoding testDecoding = new Decoding(2, "GFYKP OWTKKVJK");
+    @Test
+    public void checkIfSavesInputText()
+    {
+        assertEquals("GFYKP OWTKKVJK", testDecoding.getInputText());
+    }
 }
