626efd134b3d609027bb48f5769fbfb171758d2c
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-02-22 08:23:02 +0300

    Run Encode and Decode Tests

--- src/test/java/DecodingTest.java
+++ src/test/java/DecodingTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -5,8 +6,21 @@ import static org.junit.jupiter.api.Assertions.*;
 public class DecodingTest {
     Decoding testDecoding = new Decoding(2, "GFYKP OWTKKVJK");
     @Test
+    @DisplayName("Decode Instatiates Correctly")
+    public void checkIfItCreatesInstance()
+    {
+        assertEquals(true, testDecoding instanceof Decoding);
+    }
+    @Test
+    @DisplayName("Check if Decode stores input")
     public void checkIfSavesInputText()
     {
         assertEquals("GFYKP OWTKKVJK", testDecoding.getInputText());
+    }
+    @Test
+    @DisplayName("Check if key is captured correctly")
+    public void checkIfKeyIsCaptured ()
+    {
+        assertEquals(2, testDecoding.getKey());
     }
 }
--- src/test/java/EncodingTest.java
+++ src/test/java/EncodingTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -5,13 +6,22 @@ import static org.junit.jupiter.api.Assertions.*;
 public class EncodingTest {
     Encoding testEncoding = new Encoding ("EDWIN MURIITHI", 2);
     @Test
+    @DisplayName("Encode Instatiates Correctly")
     public void checkIfItCreatesInstance()
     {
         assertEquals(true, testEncoding instanceof Encoding);
     }
     @Test
+    @DisplayName("Check if Encode stores input")
     public void checkIfSavesInputText()
     {
         assertEquals("EDWIN MURIITHI", testEncoding.getInputText());
+    }
+
+    @Test
+    @DisplayName("Check if key is captured correctly")
+    public void checkIfKeyIsCaptured ()
+    {
+        assertEquals(2, testEncoding.getKey());
     }
 }
